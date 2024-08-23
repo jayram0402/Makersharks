5d8513ffa05e902e4f36cb5ebd9651113b6fbc2b
Jayram Sharma <147648366+jayram0402@users.noreply.github.com>
2024-08-23 22:46:26 +0530

    MakersharksApplication.java

---
+++ makersharks/src/main/java/com/example/makersharks/MakersharksApplication.java
@@ -0,0 +1,11 @@
+package com.example.makersharks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakersharksApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MakersharksApplication.class, args);
+    }
+}
